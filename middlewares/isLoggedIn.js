2fe036b84c7ddcdae3ea2b6fcec0bba9438901ff
kshitij2003 <kshitijbisht2003@gmail.com>
2024-09-29 01:01:17 +0530

    vercelchaljaoplss

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -5,7 +5,7 @@ module.exports.isLoggedIn = async function (req,res,next){
   let token = req.cookies.token;
   if(!token) return req.flash("error","you should login first") , res.redirect("/");
  try{
-  let decoded = jwt.verify(token,process.env.JWT_SECRET);
+  let decoded = jwt.verify(token,"jhfjakfkfjlsdnfjkanfjn");
   let user = await usermodel.findOne({_id:decoded._id}).select("-password");
   if(!user) return res.status(400).send("you should login first");
   req.user = user;
